1f7392fc3a71eac6a06ef9c696092565d719671d
Lucky Gill <2195905@tcs.com>
2023-02-14 16:05:40 +0530

    changes in profile info remove kid check and added constanst labels as fallback

--- constants/constant.js
+++ constants/constant.js
@@ -12,3 +12,17 @@ export const DIRECTORY_1 = "A";
 export const DIRECTORY_2 = "ENG";
 export const API_NEXT_VERSION = "1.5";
 export const PLATFORM = isMobile ? "MWEB" : "WEB";
+
+
+
+/*****************Profile setup screen label *******************/
+
+export const profile_setup_title = 'Tell us about yourself';
+export const profile_setup_choose_avatar_title = 'Choose avatar';
+export const profile_setup_name_placeholder = 'Name';
+export const profile_setup_kids_mode_title = "kid's mode";
+export const profile_setup_dob_placeholder = 'Date of birth (DD/MM/YYYY)';
+export const profile_setup_select_gender_title = 'What do you identify as?';
+export const profile_setup_select_languages_title = 'Choose your preferred content language ';
+export const profile_setup_create_cta = 'Create Profile';
+/*************************************************************/
