39a81c5e761539e89a11244f8961b0d285e9e129
Bhushan Mehta <bhushan.neo@gmail.com>
2023-02-16 18:01:23 +0530

    made changes to test LightHouse report

--- pages/test.js
+++ pages/test.js
@@ -1,10 +1,22 @@
 import SignIn from "@/components/SignIn/SignIn";
+import Head from "next/head";
 
 const { default: ConfirmOtp } = require("@/components/ConfirmOtp/ConfirmOtp");
 
 function TestLight() {
     return <>
-        <ConfirmOtp />
+        <Head>
+        <title>Player Page</title>
+        <meta charset="UTF-8" />
+        <meta name="keywords" content="titla, meta, nextjs" />
+        <meta name="author" content="Syamlal CM" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content=" Player page"
+        />
+      </Head>
+    <SignIn />
     </>
 }
 
